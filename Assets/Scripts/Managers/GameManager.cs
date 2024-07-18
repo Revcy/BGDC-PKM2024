a57a4bd672a1f585bd222a19335a7b52311ee178
Revcy <Revcy.unity@gmail.com>
2024-07-18 17:03:59 +0700

    MainMenu Framework & More
    Added:
    3 Scenes to work on (MainMenu, Tutorial, Level 1)
    Main Menu assets without BG
    GameManager script (Incomplete)
    Main Menu buttons and functionality

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+public enum GameState { MainMenu, Playing, Dead, Paused, Loading, PopupOpen }
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public static GameState currentState;
+    public float backToMenuDelay;
+    public bool isJournaling = false;
+
+    public static event Action OnBackToMenu;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+            instance = this;
+
+        DontDestroyOnLoad(gameObject);
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+            SceneManager.LoadScene(0);
+    }
+
+    private void Start()
+    {
+        currentState = GameState.MainMenu;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && currentState != GameState.MainMenu && currentState != GameState.PopupOpen && currentState != GameState.Loading)
+        {
+            if (currentState == GameState.Paused)
+                Resume();
+            else if (currentState == GameState.Playing)
+                Pause();
+        }
+    }
+
+    public IEnumerator BackToMenu()
+    {
+        Time.timeScale = 1f;
+        OnBackToMenu?.Invoke();
+        currentState = GameState.Loading;
+        Debug.Log("BackToMenu");
+
+        yield return new WaitForSeconds(backToMenuDelay);
+        SceneManager.LoadScene(0);
+
+        //WinCanvasManager.Instance.SetWinScreen(false);
+        //SoundManager.instance.StopAllSounds();
+        //DeathScreen.instance.SetDeathTrue(false);
+        currentState = GameState.MainMenu;
+    }
+
+    public void Pause()
+    {
+        //SoundManager.instance.PauseAllSounds(true);
+        //PauseMenu.instance.SetPauseActive(true);
+        currentState = GameState.Paused;
+        Time.timeScale = 0f;
+    }
+    public void Journaling()
+    {
+        isJournaling = !isJournaling;
+        if (isJournaling) Pause();
+        else Resume();
+    }
+    public void Resume()
+    {
+        //SoundManager.instance.PauseAllSounds(false);
+        //PauseMenu.instance.SetPauseActive(false);
+        currentState = GameState.Playing;
+        Time.timeScale = 1;
+    }
+
+    public void Death()
+    {
+        if (currentState == GameState.Dead) return;
+
+        Time.timeScale = 0;
+        currentState = GameState.Dead;
+        Debug.Log("Player ran out of time");
+        //SoundManager.instance.PlaySoundEffect(SFXindex);
+        //DeathScreen.instance.SetDeathTrue(true);
+    }
+
+    [ContextMenu("Remove All Player Prefs")]
+    public void RemoveAllPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
+
+
+
---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject optionsCanvas;
+    public GameObject creditsCanvas;
+
+    public void Play()
+    {
+        //SceneManager.LoadScene("Tutorial");
+        Debug.Log("Play Clicked");
+    }
+
+    public void Options()
+    {
+        optionsCanvas.SetActive(true);
+        Debug.Log("Options Clicked");
+    }
+
+    public void Credits()
+    {
+        creditsCanvas.SetActive(true);
+        Debug.Log("Credits Clicked");
+    }
+
+    public void Exit()
+    {
+       //Application.Quit();
+       Debug.Log("Exit Clicked");
+    }
+}
