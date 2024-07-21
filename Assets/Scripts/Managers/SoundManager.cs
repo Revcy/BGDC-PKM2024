dd2b18509e7c73fd620ced0d5d307d6e45dc2ee9
scarletrosez <117636456+scarletrosez@users.noreply.github.com>
2024-07-21 22:23:23 +0700

    Music and SFX Settings
    - Added slider to set volume of music and sfx
    - Use PlayerPrefs to save volume settings

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,29 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance;
-    
-    private void Awake(){
-        if(instance == null){
+
+    public AudioSource musicSource, sfxSource;
+    public Slider musicSlider, sfxSlider;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
             instance = this;
             DontDestroyOnLoad(gameObject);
+            LoadVolume();
         }
-        else{
+        else
+        {
             Destroy(gameObject);
         }
     }
 
-    public AudioSource musicSource, sfxSource;
+    private void Start()
+    {
+        // Initialize the sliders' values and add listeners
+        if (musicSlider != null)
+        {
+            musicSlider.value = musicSource.volume;
+            musicSlider.onValueChanged.AddListener(delegate { SetMusicVolume(musicSlider.value); });
+        }
 
-    public void PlaySFX(AudioClip clip){
+        if (sfxSlider != null)
+        {
+            sfxSlider.value = sfxSource.volume;
+            sfxSlider.onValueChanged.AddListener(delegate { SetSFXVolume(sfxSlider.value); });
+        }
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
         sfxSource.PlayOneShot(clip);
     }
 
-    public void PlayMusic(AudioClip clip){
+    public void PlayMusic(AudioClip clip)
+    {
         musicSource.clip = clip;
         musicSource.Play();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            musicSource.volume = PlayerPrefs.GetFloat("MusicVolume");
+        }
+        else
+        {
+            musicSource.volume = 1f; // Default volume
+        }
+
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            sfxSource.volume = PlayerPrefs.GetFloat("SFXVolume");
+        }
+        else
+        {
+            sfxSource.volume = 1f; // Default volume
+        }
     }
 }
