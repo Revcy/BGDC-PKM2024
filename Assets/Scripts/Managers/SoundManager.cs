a320d47bc6497fbf8a9df0b41bfc7cef7f1a4401
scarletrosez <117636456+scarletrosez@users.noreply.github.com>
2024-07-21 21:54:41 +0700

    Sound Manager
    Added Sound Manager Script
    - To play music
    - To play sfx

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    
+    private void Awake(){
+        if(instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+
+    public AudioSource musicSource, sfxSource;
+
+    public void PlaySFX(AudioClip clip){
+        sfxSource.PlayOneShot(clip);
+    }
+
+    public void PlayMusic(AudioClip clip){
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+}
